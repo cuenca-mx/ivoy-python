fe49481fa688dfd44f58e809e148a616d11e655a
Andrès Hernández <andreshernandez@MacBook-Pro-de-Andres.local>
2019-11-29 18:24:07 -0600

    Resolving comments

--- ivoy/client.py
+++ ivoy/client.py
@@ -6,8 +6,8 @@ from requests import Response, Session
 from .exc import ExpiredTokens, IvoyException
 from .resources import Budget, Order, OrderSharing, Resource
 
-API_URL = os.environ["IVOY_URL"]
-WEB_URL = os.environ["IVOY_WEB_URL"]
+API_URL = os.environ['IVOY_URL']
+WEB_URL = os.environ['IVOY_WEB_URL']
 
 
 class Client:
@@ -39,51 +39,51 @@ class Client:
         web_auth_password: Optional[str] = None,
     ):
         self.session = Session()
-        self.auth_user = auth_user or os.environ["AUTH_USER"]
-        self.auth_password = auth_password or os.environ["AUTH_PASS"]
-        self.web_auth_user = web_auth_user or os.environ["WEB_AUTH_USER"]
+        self.auth_user = auth_user or os.environ['AUTH_USER']
+        self.auth_password = auth_password or os.environ['AUTH_PASS']
+        self.web_auth_user = web_auth_user or os.environ['WEB_AUTH_USER']
         self.web_auth_password = (
-            web_auth_password or os.environ["WEB_AUTH_PASS"]
+            web_auth_password or os.environ['WEB_AUTH_PASS']
         )
-        self.ivoy_user = ivoy_user or os.environ["IVOY_USER"]
-        self.ivoy_password = ivoy_password or os.environ["IVOY_PASS"]
+        self.ivoy_user = ivoy_user or os.environ['IVOY_USER']
+        self.ivoy_password = ivoy_password or os.environ['IVOY_PASS']
         self.token = self.get_token()
         self.web_token = self.get_token(True)
         Resource._client = self
 
     def get_token(self, web_token: bool = False) -> str:
-        url = f"{self.base_url}/api/login/loginClient/json/web"
+        url = f'{self.base_url}/api/login/loginClient/json/web'
         if web_token:
             auth = (self.web_auth_user, self.web_auth_password)
         else:
             auth = (self.auth_user, self.auth_password)
         response = self.session.request(
-            "POST",
+            'POST',
             url,
             auth=auth,
             json={
-                "data": {
-                    "systemRequest": {
-                        "user": self.ivoy_user,
-                        "password": self.ivoy_password,
+                'data': {
+                    'systemRequest': {
+                        'user': self.ivoy_user,
+                        'password': self.ivoy_password,
                     }
                 }
             },
         )
         self._check_response(response)
         data = response.json()
-        return data["token"]["access_token"]
+        return data['token']['access_token']
 
     def post(self, endpoint: str, **kwargs: Any) -> Dict[str, Any]:
-        return self.request("post", endpoint, **kwargs)
+        return self.request('post', endpoint, **kwargs)
 
     def put(self, endpoint: str, **kwargs: Any) -> Dict[str, Any]:
-        return self.request("put", endpoint, **kwargs)
+        return self.request('put', endpoint, **kwargs)
 
     def request(
         self, method: str, endpoint: str, **kwargs: Any,
     ) -> Dict[str, Any]:
-        if "orderSharing" in endpoint:
+        if 'orderSharing' in endpoint:
             url = self.web_url + endpoint
         else:
             url = self.base_url + endpoint
@@ -93,6 +93,7 @@ class Client:
         try:
             self._check_response(response)
         except ExpiredTokens:
+            # Try to generate Tokens again and retry request
             self.token = self.get_token()
             self.web_token = self.get_token(True)
             headers = self.create_headers(endpoint)
@@ -108,29 +109,29 @@ class Client:
         return response.json()
 
     def create_headers(self, endpoint: str) -> Dict[str, Any]:
-        if "orderSharing" in endpoint:
+        if 'orderSharing' in endpoint:
             return {
-                "Content-type": "application/json",
-                "Token": self.web_token,
+                'Content-type': 'application/json',
+                'Token': self.web_token,
             }
         else:
             return {
-                "Content-type": "application/json",
-                "Token": self.token,
+                'Content-type': 'application/json',
+                'Token': self.token,
             }
 
     @staticmethod
     def _check_response(response: Response) -> None:
         if response.ok:
             json = response.json()
-            if "code" in json and json["code"] == 0:
+            if 'code' in json and json['code'] == 0:
                 return
-            elif "code" in json and json["code"] == -156:
-                # Tokens expired
-                print("Expirado")
-                raise ExpiredTokens(156, "Tokens have expired.")
+            elif 'code' in json and json['code'] == -156:
+                # Tokens expired. Based on http://docs.ivoy.mx/express/#errores
+                # All error codes come from API as negative numbers.
+                raise ExpiredTokens(156, 'Tokens have expired.')
             else:
                 raise IvoyException(
-                    json["code"], "iVoy API error: {}".format(json["message"])
+                    json['code'], 'iVoy API error: {}'.format(json['message'])
                 )
         response.raise_for_status()
