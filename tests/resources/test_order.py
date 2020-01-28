7b3ce864567a8f9f9da495ed91c134f97ab88d69
JessicaShalomG <jessica@cuenca.com>
2020-01-28 11:33:54 -0600

    added mores test

--- tests/resources/test_order.py
+++ tests/resources/test_order.py
@@ -38,50 +38,16 @@ def order_info():
         street='Sonora',
         zip_code='06100',
     )
-
-    origin_badNumber = OrderAddress(
-        is_pickup=1,
-        is_source=1,
-        person_approved='Jon Doe',
-        phone='not a number',
-        id_address=None,
-        external_number='36',
-        internal_number=None,
-        comment='N/A',
-        latitude='19.4245207',
-        longitude='-99.1676911',
-        neighborhood='Juarez',
-        street='Varsovia',
-        zip_code='06600',
-    )
-
-    destiny_badNumber = OrderAddress(
-        is_pickup=0,
-        is_source=0,
-        person_approved='Lewis Hamilton',
-        phone='',
-        id_address=None,
-        external_number='180',
-        internal_number='1',
-        comment='N/A',
-        latitude='19.4133562',
-        longitude='-99.1672142',
-        neighborhood='Hipodromo',
-        street='Sonora',
-        zip_code='06100',
-    )
-    bad_number_addresses = [origin_badNumber, destiny_badNumber]
-    adresses = [origin, destiny]
+    addresses = [origin, destiny]
     bad_addresses = [origin]
     payment = PaymentMethod.credit_prepaid
     package = Package.envelope
 
     return dict(
-        adresses=adresses,
+        addresses=addresses,
         bad_addresses=bad_addresses,
         payment=payment,
         package=package,
-        bad_number_addresses=bad_number_addresses,
     )
 
 
@@ -90,7 +56,7 @@ def test_order_create():
     client = Client()
     info = order_info()
     order = client.order.create(
-        info['adresses'], info['package'], info['payment']
+        info['addresses'], info['package'], info['payment']
     )
     assert order
     assert type(order) == Order
@@ -115,7 +81,7 @@ def test_order_retrieve():
     client = Client()
     info = order_info()
     order_created = client.order.create(
-        info['adresses'], info['package'], info['payment']
+        info['addresses'], info['package'], info['payment']
     )
     order = client.order.retrieve(order_created.id)
     assert order
@@ -129,7 +95,9 @@ def test_order_retrieve_http_500_error():
     info = order_info()
 
     with pytest.raises(HTTPError) as ex:
-        client.order.create(info['adresses'], info['package'], info['payment'])
+        client.order.create(
+            info['addresses'], info['package'], info['payment']
+        )
     assert ex.value.response.status_code == 500
 
 
@@ -150,7 +118,7 @@ def test_order_cancel():
     client = Client()
     info = order_info()
     order_created = client.order.create(
-        info['adresses'], info['package'], info['payment']
+        info['addresses'], info['package'], info['payment']
     )
     order = client.order.cancel(order_created.id)
     assert order
@@ -177,7 +145,7 @@ def test_order_create_bad_phone_number():
     info = order_info()
     try:
         client.order.create(
-            info['bad_number_addresses'], info['package'], info['payment']
+            info['addresses'], info['package'], info['payment']
         )
     except exc.InvalidPhone as e:
         assert client
@@ -190,7 +158,9 @@ def test_not_available():
     client = Client()
     info = order_info()
     try:
-        client.order.create(info['adresses'], info['package'], info['payment'])
+        client.order.create(
+            info['addresses'], info['package'], info['payment']
+        )
     except exc.NotAvailable as e:
         assert client
         assert e.code == -160
@@ -202,8 +172,38 @@ def test_invalid_code():
     client = Client()
     info = order_info()
     try:
-        client.order.create(info['adresses'], info['package'], info['payment'])
+        client.order.create(
+            info['addresses'], info['package'], info['payment']
+        )
     except exc.InvalidCode as e:
         assert client
         assert e.code == -124
         assert e.message == f'This code is not valid or already used'
+
+
+@pytest.mark.vcr
+def test_invalid_vehicle():
+    client = Client()
+    info = order_info()
+    try:
+        client.order.create(
+            info['addresses'], info['package'], info['payment']
+        )
+    except exc.InvalidVehicle as e:
+        assert client
+        assert e.code == -258
+        assert e.message == f'Error on vehicle or not available'
+
+
+@pytest.mark.vcr
+def test_missing_incomplete_information():
+    client = Client()
+    info = order_info()
+    try:
+        client.order.create(
+            info['addresses'], info['package'], info['payment']
+        )
+    except exc.MissingInformation as e:
+        assert client
+        assert e.code == -199
+        assert e.message == f'Incomplete or missing information'
