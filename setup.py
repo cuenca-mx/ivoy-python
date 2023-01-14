acb85de3756b292fcaef6c16e17f749d19ec6e76
Andres Hernandez <andres@cuenca.com>
2023-01-13 18:05:33 -0600

    hope this works

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ test_requires = [
     'pytest-vcr==1.0.2',
     'pycodestyle==2.6.0',
     'pytest-cov==2.10.0',
-    'black==22.12.0',
+    'black==19.10b0',
     'isort[pipfile]==4.3.21',
-    'flake8==3.8.3',
+    'flake8==3.7.*',
     'mypy==0.782',
     'iso8601==0.1.12',
 ]
